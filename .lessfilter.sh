487a0538ae2db246679a67dfb24ce06d6c4c2b15
Arseny Mitin <mitinarseny@gmail.com>
2018-12-06 13:45:35 +0300

    add pygments less support

--- .lessfilter.sh
+++ .lessfilter.sh
@@ -3,11 +3,11 @@ case "$1" in
     .bashrc|.bash_aliases|.bash_environment|.zshrc)
         pygmentize -f 256 -l sh -O style=monokai "$1";;
 	.py)
-		pygmentize -f 256 -l py3 "$1";;
+		pygmentize -f 256 -l py3 -O style=monokai "$1";;
 
     *)
         if grep -q "#\!/bin/bash" "$1" 2> /dev/null; then
-            pygmentize -f 256 -l sh "$1"
+            pygmentize -f 256 -l sh -O style=monokai "$1"
         else
 			pygmentize -f 256 -O style=monokai "$1"
         fi

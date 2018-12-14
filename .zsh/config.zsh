20af40487a438193f1a6421c5bedd68bc90806a3
Arseny Mitin <mitinarseny@gmail.com>
2018-12-15 02:36:23 +0300

    fix: rm textmate

--- .zsh/config.zsh
+++ .zsh/config.zsh
@@ -6,8 +6,5 @@ setopt HIST_BEEP			# Beep when accessing non-existent history.
 setopt AUTO_CD              # Auto changes to a directory without typing cd.
 setopt AUTO_PUSHD			# Push the old directory onto the stack on cd.
 
-# Enable notifications
-#test -e ~/.antigen/bundles/marzocchi/zsh-notify/notify.plugin.zsh && source ~/.antigen/bundles/marzocchi/zsh-notify/notify.plugin.zsh
-
 # Enable iTerm Shell Integration
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"

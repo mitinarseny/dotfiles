0ec2c9b6feef7599829198966b0a4fa2fd97f42b
Arseny Mitin <mitinarseny@gmail.com>
2019-06-21 02:10:01 +0300

    fix: less man pages colors

--- .zsh/config.zsh
+++ .zsh/config.zsh
@@ -31,21 +31,12 @@ export LESS='-iwMRj4a#4z-4'
 export PAGER="less -F"
 
 # Less Colors for Man Pages
-# Source: https://github.com/Valloric/dotfiles/blob/master/less/LESS_TERMCAP
+# Source: https://boredzo.org/blog/archives/2016-08-15/colorized-man-pages-understood-and-customized
 # More info: http://unix.stackexchange.com/a/108840
-export LESS_TERMCAP_mb=$(tput bold; tput setaf 2) # green
-export LESS_TERMCAP_md=$(tput bold; tput setaf 3) # yellow
+# Termcap: https://linux.die.net/man/5/termcap
+# tput: http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/x405.html
+export LESS_TERMCAP_md=$(tput bold; tput setaf 3)
 export LESS_TERMCAP_me=$(tput sgr0)
-export LESS_TERMCAP_so=$(tput bold; tput setaf 0; tput setab 3) # transparent on yellow
-export LESS_TERMCAP_se=$(tput rmso; tput sgr0)
-export LESS_TERMCAP_us=$(tput smul; tput bold; tput setaf 7) # white
-export LESS_TERMCAP_ue=$(tput rmul; tput sgr0)
-export LESS_TERMCAP_mr=$(tput rev)
-export LESS_TERMCAP_mh=$(tput dim)
-export LESS_TERMCAP_ZN=$(tput ssubm)
-export LESS_TERMCAP_ZV=$(tput rsubm)
-export LESS_TERMCAP_ZO=$(tput ssupm)
-export LESS_TERMCAP_ZW=$(tput rsupm)
 
 # Matplotlib inline graphics support
 export MPLBACKEND="module://itermplot"

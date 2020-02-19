4eb2ad40b67ff42012d30694ca88b3bf819c6c83
Arseny Mitin <mitinarseny@gmail.com>
2020-02-19 23:32:19 +0300

    fix: fzf colors

--- fzf/config.zsh
+++ fzf/config.zsh
@@ -5,11 +5,10 @@ if command -v fd > /dev/null 2>&1; then
   export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
 fi
 
-export FZF_DEFAULT_OPTS="
-  --preview='(([[ -d {} ]] && tree -CFv --dirsfirst --noreport -L 2 {}) || (bat --decorations always --color always --style=plain {} || cat {}) 2> /dev/null) | head -n 200'
-  --color fg:#D8DEE9,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C
-  --color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B
-  --cycle
-  --info=inline
-  -1 -0
-"
+export FZF_DEFAULT_OPTS="\
+  --preview='(([[ -d {} ]] && tree -CFv --dirsfirst --noreport -L 2 {}) || (bat --decorations always --color always --style=plain {} || cat {}) 2> /dev/null) | head -n 200' \
+  --color hl:2,bg+:8,hl+:2 \
+  --color pointer:6,info:8,spinner:8,header:8,prompt:1,marker:1 \
+  --cycle \
+  --info=inline \
+  -1 -0"

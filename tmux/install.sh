179522319b819e12d7c6fb767509a5d32c1fe216
Arseny Mitin <mitinarseny@gmail.com>
2020-02-19 22:54:03 +0300

    fix: install tmux plugin manager in tmux.conf, not in install.sh

--- tmux/install.sh
+++ tmux/install.sh
@@ -2,9 +2,3 @@
 
 ln -si $(cd "$(dirname "$0")" && pwd)/tmux.conf ${HOME}/.tmux.conf
 
-command -v tmux > /dev/null 2>&1 || (echo 'tmux is not in ${PATH}'; exit 1)
-
-[ ! -d "${HOME}/.tmux/plugins/tpm" ] \
-  && git clone https://github.com/tmux-plugins/tpm "${HOME}/.tmux/plugins/tpm"
-
-${HOME}/.tmux/plugins/tpm/bin/install_plugins

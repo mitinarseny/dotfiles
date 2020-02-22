9dfd0b1b9a79a053fdfecd70439d3b0530cd0b21
Arseny Mitin <mitinarseny@gmail.com>
2020-02-22 20:39:51 +0300

    feat: determine main tmux session name

--- tmux/path.zsh
+++ tmux/path.zsh
@@ -1,1 +1,1 @@
-alias t="tmux has-session 2> /dev/null && tmux attach-session\; display-message 'attached: #{session_name}' || tmux new-session\; display-message 'created: #{session_name}'"
+alias tm='tmux new-session -A -s main'

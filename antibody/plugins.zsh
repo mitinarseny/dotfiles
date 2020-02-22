f9f0faff1558247380bd782c21d1643f5ed1357f
Arseny Mitin <mitinarseny@gmail.com>
2020-02-22 20:14:01 +0300

    feat: zsh autostart tmux, zshrc optimized, plugins settings are in antobody/plugins.zsh

---
+++ antibody/plugins.zsh
@@ -0,0 +1,6 @@
+####### zdharma/fast-syntax-highlighting #######
+# https://github.com/zdharma/fast-syntax-highlighting/issues/135#issuecomment-497452498
+export FAST_HIGHLIGHT[whatis_chroma_type]=0
+
+####### sindresorhus/pure #######
+export PURE_PROMPT_SYMBOL='λ'
--- tmux/path.zsh
+++ tmux/path.zsh
@@ -1,1 +1,1 @@
-alias t='tmux attach-session || tmux new-session\; split-window -h'
+alias t="tmux has-session 2> /dev/null && tmux attach-session\; display-message 'attached: #{session_name}' || tmux new-session\; display-message 'created: #{session_name}'"

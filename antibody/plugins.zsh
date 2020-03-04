f8b7df40c4a8900a36f526667de90bb0321122f2
Arseny Mitin <mitinarseny@gmail.com>
2020-03-04 21:00:28 +0300

    fix: no vi cmd mode in zsh, pure theme in fpath

--- antibody/plugins.zsh
+++ antibody/plugins.zsh
@@ -1,9 +1,11 @@
+####### sindresorhus/pure #######
+autoload -U promptinit; promptinit
+prompt pure
+export PURE_PROMPT_SYMBOL='λ'
+
 ####### zdharma/fast-syntax-highlighting #######
 # https://github.com/zdharma/fast-syntax-highlighting/issues/135#issuecomment-497452498
 export FAST_HIGHLIGHT[whatis_chroma_type]=0
-
-####### sindresorhus/pure #######
-export PURE_PROMPT_SYMBOL='λ'
 
 ####### zsh-users/zsh-history-substring-search #######
 [[ -n "${key[Up]}"   ]] && bindkey -- "${key[Up]}"   history-substring-search-up

53fefbbe64476064179054d054d33dff412df554
Arseny Mitin <mitinarseny@gmail.com>
2020-03-18 00:12:08 +0300

    fix: do not use terminfo for bindkey

--- antibody/plugins.zsh
+++ antibody/plugins.zsh
@@ -12,8 +12,8 @@ ZSH_AUTOSUGGEST_USE_ASYNC=1
 export FAST_HIGHLIGHT[whatis_chroma_type]=0
 
 ####### zsh-users/zsh-history-substring-search #######
-[[ -n "${key[Up]}"   ]] && bindkey -- "${key[Up]}"   history-substring-search-up
-[[ -n "${key[Down]}" ]] && bindkey -- "${key[Down]}" history-substring-search-down
+bindkey '\e[A'   history-substring-search-up
+bindkey '\e[B' history-substring-search-down
 typeset -g HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='fg=black,bg=cyan'
 typeset -g HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='fg=white,bg=red'
 

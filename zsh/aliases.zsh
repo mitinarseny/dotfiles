8709858124faccd48f9ddbb06b5cdb55ecb22a78
Arseny Mitin <mitinarseny@gmail.com>
2020-04-07 01:09:29 +0300

    fix(zsh): detect ls version for colors

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,11 @@
 alias tree='tree -C'
 alias tb="nc termbin.com 9999"
-alias ls='ls --color=auto' # enable colors automatically
+
+if ls --color > /dev/null 2>&1; then # GNU `ls`
+  alias ls='ls --color=auto' # enable colors automatically
+else # macOS `ls`
+  alias ls='ls -G'
+fi
 alias l='ls -lFh'     #size,show type,human readable
 alias la='ls -lAFh'   #long list,show almost all,show type,human readable
 alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
@@ -10,6 +15,9 @@ alias ldot='ls -ld .*'
 alias lS='ls -1FSsh'
 alias lart='ls -1Fcart'
 alias lrt='ls -1Fcrt'
+
 alias zshrc='${=EDITOR} ~/.zshrc' # Quick access to the ~/.zshrc file
+
 alias grep='grep --color'
+
 alias groot='cd $(git rev-parse --show-toplevel)' # go to nearest directory containing .git/

4f4a150e8a7e8078afdc560f51aa2a492a08d666
Arseny Mitin <mitinarseny@gmail.com>
2020-04-10 23:28:18 +0300

    feat(fzf): respect fd colors

--- fzf/config.zsh
+++ fzf/config.zsh
@@ -1,10 +1,5 @@
 FZF_COMPLETION_TRIGGER=','
 
-if command -v fd > /dev/null 2>&1; then
-  export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
-  FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
-fi
-
 export FZF_DEFAULT_OPTS="\
   --color hl:2,bg+:8,hl+:2 \
   --color pointer:6,info:8,spinner:8,header:8,prompt:1,marker:1 \
@@ -14,3 +9,9 @@ export FZF_DEFAULT_OPTS="\
   --bind bspace:backward-delete-char/eof \
   --height=30% \
 "
+
+if command -v fd > /dev/null 2>&1; then
+  export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git --color=always'
+  FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
+  export FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} --ansi"
+fi

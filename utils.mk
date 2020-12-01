e16a8ddac3cca36fb58a8502cbfbda3e6292d389
Arseny Mitin <mitinarseny@gmail.com>
2020-12-02 00:11:42 +0300

    rm rudiments

--- utils.mk
+++
@@ -1,6 +0,0 @@
-DOTFILES_ROOT := $(patsubst %/,%,$(dir $(realpath $(lastword $(MAKEFILE_LIST)))))
-
-UNAME := $(shell uname -s)
-ifeq (Darwin,$(UNAME))
-include $(DOTFILES_ROOT)/macos/brew.mk
-endif

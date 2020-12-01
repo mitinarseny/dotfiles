9d763e5b32efe39a523d0573cb6e95051ce2c199
Arseny Mitin <mitinarseny@gmail.com>
2020-12-02 00:25:47 +0300

    fix(ci): check if CI before antibody install

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,14 +3,14 @@
 cd "$(dirname "$0")"
 ln -sfv $(readlink -ev zshrc) ~/.zshrc
 
+# check if CI
+[ -z "${CI+x}" ] && exit
+
 if ! command -v antibody > /dev/null; then
   curl --fail --silent --show-error --location git.io/antibody | sh -s - -b /usr/local/bin
 fi
 
 antibody bundle < plugins.txt > ~/.zsh_plugins.sh
-
-# check if CI
-[ -z "${CI+x}" ] && exit
 
 if ! grep "$(command -v zsh)" /etc/shells > /dev/null; then
   sudo sh -c "echo $(command -v zsh) >> /etc/shells"

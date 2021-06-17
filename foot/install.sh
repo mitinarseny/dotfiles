8d6354c43b58428ac09c519c0eecbc4119a0bd46
Arseny Mitin <mitinarseny@gmail.com>
2021-06-18 01:48:24 +0300

    feat(foot): initial config

---
+++ foot/install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -e
+
+CONFIG_DIR=${XDG_CONFIG_DIR:-"${HOME}/.config"}/foot
+[ -d "${CONFIG_DIR}" ] || mkdir -p "${CONFIG_DIR}"
+
+cd "$(dirname "$0")"
+
+ln -svf "$(pwd -P)"/foot.ini "${CONFIG_DIR}"

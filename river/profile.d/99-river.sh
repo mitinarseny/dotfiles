3604c8a1db6d54390a25fd90c03c45c69312ab08
Arseny Mitin <mitinarseny@gmail.com>
2022-04-14 15:37:07 +0300

    fix(river): screensharing: dbus should know XDG_CURRENT_DESKTOP and WAYLAND_DISPLAY variables

--- river/profile.d/99-river.sh
+++ river/profile.d/99-river.sh
@@ -1,9 +1,9 @@
 if command -v river > /dev/null && [ -z "${DISPLAY}" ] && [ -z "${WAYLAND_DISPLAY}" ] && [ "$(tty)" = '/dev/tty1' ]; then
+  export SVDIR=${HOME}/.local/sv
+  export XDG_SESSION_TYPE=wayland
+  export XDG_CURRENT_DESKTOP=river
   exec dbus-run-session -- \
     env \
-      SVDIR=${HOME}/.local/sv \
-      XDG_SESSION_TYPE=wayland \
-      XDG_CURRENT_DESKTOP=river \
       XKB_DEFAULT_LAYOUT='us,ru' \
       XKB_DEFAULT_OPTIONS='grp:alt_space_toggle' \
     river

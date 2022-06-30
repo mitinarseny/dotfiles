a0eb09c8f196ac45ff2172d60e3829c8e22d0c0c
Arseny Mitin <mitinarseny@gmail.com>
2022-06-30 19:28:19 +0300

    feat(nvim): autopairs

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,9 +1,16 @@
 require('nvim-treesitter.configs').setup({
   ensure_installed = {
-    'c',
-    'lua',
+    'c', 'cpp',
+    'dockerfile',
     'go',
+    'gomod',
+    'json', 'json5',
+    'lua',
+    'make',
+    'markdown',
+    'python',
     'rust',
+    'toml',
   },
   highlight = {
     enable = true,

63ab1ae03bc9ba33478fa0f274bc46243d79f27b
Arseny Mitin <mitinarseny@gmail.com>
2023-03-23 12:28:22 +0200

    feat(shh): use apple keychain

--- .profile.d/70-ssh-agent.sh
+++ .profile.d/70-ssh-agent.sh
@@ -1,4 +1,7 @@
 if command -v ssh-agent > /dev/null && [ -z "${SSH_AUTH_SOCK}" ]; then
   eval "$(ssh-agent -s)" > /dev/null
   trap '[ -n "${SSH_AGENT_PID}" ] && eval $(ssh-agent -k)' 0
+  if command -v ssh-add > /dev/null && [ "$(uname)" -eq 'Darwin' ]; then
+    ssh-add --apple-load-keychain
+  fi
 fi

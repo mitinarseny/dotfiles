1a64e9d4eb38d0a5ed4253b614951722032da319
Arseny Mitin <mitinarseny@gmail.com>
2025-10-26 22:51:02 +0400

    fix(ssh): apple load keychain in background

--- .profile.d/70-ssh-agent.sh
+++ .profile.d/70-ssh-agent.sh
@@ -1,7 +1,8 @@
 if command -v ssh-agent > /dev/null && [ -z "${SSH_AUTH_SOCK}" ]; then
   eval "$(ssh-agent -s)" > /dev/null
   trap '[ -n "${SSH_AGENT_PID}" ] && eval $(ssh-agent -k)' 0
-  if command -v ssh-add > /dev/null && [ "$(uname)" -eq 'Darwin' ]; then
-    ssh-add --apple-load-keychain -q
-  fi
+fi
+
+if command -v ssh-add > /dev/null && [ "$(uname)" == 'Darwin' ]; then
+  ssh-add --apple-load-keychain -q 2>/dev/null &
 fi
